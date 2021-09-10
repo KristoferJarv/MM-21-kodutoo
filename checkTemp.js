e290eb3acf3879fabd85858192fe8d1e1f0526db
Kristofer Järv <90023688+KristoferJarv@users.noreply.github.com>
2021-09-10 11:05:44 +0300

    checkTemp.js

--- checkTemp.js
+++ checkTemp.js
@@ -60,26 +60,57 @@ typeof(summa)
 // user defined types:
 // Math 
 
-
+//enum
 
 
 /*
   Katlal on kolm temoperatuurivahemikku
-  0-20 on liiga külm
-  21-40 on paras temperatuur
-  41-60 on liiga kuum
+  0-20 on liiga külm          -1
+  21-40 on paras temperatuur   0 
+  41-60 on liiga kuum          1 
   GitHubi: homework/checkTemp.js
 */
-function checkTemp () {
-      let temp = 5;
-
-      if (temp < 0) {
+function checkTemp (temp) {
+      console.log("temp on "+temp)
+      if (temp < 20) {
           console.log("Liiga Külm")
-      } else if (temp > 20 && temp <=40) {
-          console.log("paras temperatuur")
+          return -1;
+      } else if (temp <=40) {
+        console.log("paras temperatuur")
+          return 0;
 
-      } else if (temp > 60) {
-        console.log ("liiga kuum")
+      } else {
+        console.log("liiga kuum")
+          return 1;
+       
       }
 
   }
+
+checkTemp(5)
+checkTemp(15)
+checkTemp(30)
+checkTemp(45)
+checkTemp(55)
+
+
+function checkTemp() {
+  let Temp = 5;
+  temp = Math.random()*60 
+}
+
+
+  function summa(a,b ) {
+      return a+b
+  }
+let Vastus = summa(2,7)
+
+function kasKütta(temp) {
+  return temp < 20
+}
+
+function kasJahutada(temp) {
+    returntemp > 40
+}
+
+let kasJahutada = true;
