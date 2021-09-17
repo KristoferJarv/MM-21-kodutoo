7bf00a95eec7fafdf8538648313bf9bf4662f2fe
Kristofer Järv <90023688+KristoferJarv@users.noreply.github.com>
2021-09-17 09:18:38 +0300

    Update 17.09.js

--- 17.09.js
+++ 17.09.js
@@ -1,4 +1,24 @@
+Array []
 let tahaTeada = prompt ("Mis kuu")
 let et = [10,12,15,13,15,11,22,31,17,21,10,19];
 console.log(et)
-console.log(et[tahaTeada])
+console.log(et[tahaTeada-1])
+
+
+
+let tahaTeada = prompt ("Mis kuu")
+let et = [10,12,15,13,18,11,22,31,17,21,10,19];
+let kuu = ["Jan", "Veb", "Mar", "Apr", "Mai", "Jun"];
+console.log(kuu[0]+" tarbimine on "+et[tahaTeada-1]);
+
+
+
+
+
+
+
+
+let tahaTeada = prompt ("Mis kuu")
+let et = [1,2,3,4,5,6,7,8,9,10,11,12];
+console.log(et)
+console.log(et[tahaTeada]) 
