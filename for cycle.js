aa7ad021c3b35f8fc8cf26e132ca93f53169ee4f
Kristofer Järv <90023688+KristoferJarv@users.noreply.github.com>
2021-10-01 10:34:41 +0300

    for cycle.js

--- for cycle.js
+++ for cycle.js
@@ -11,3 +11,73 @@ while(x <= 100) {
     console.log(x);
     x++
 }
+
+i + 1
+i = i+7
+i-=5
+
+for (let)
+
+while (count > 1) {
+    console.log(count)
+count =50
+
+let i = 0;
+do {
+  i += 1;
+  console.log(i);
+} while (i < 50);
+
+let arr = [5,8,4,3,9,1];
+for (let i=0; i <= arr.length; i++) {
+    console.log(i, arr[1]) 
+    arr[i]++
+}
+
+
+let arr = [5,8,4,3,9,1];
+for (let i = 0; i <= arr.length; i++) { 
+    arr[i]++
+}
+
+
+
+let arr = [5,8,4,2,3,1];
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] > 4) {
+    console.log(arr[i])
+  } else {
+    
+  }
+}
+
+
+
+
+let arr = [5,8,4,2,3,1];
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] > 4) {
+    console.log("Läks esimene")
+  } else {
+        console.log("Läks teine")
+    
+  }
+}
+
+
+
+
+
+
+
+
+
+
+const data = ["FWmcfgAWxe","KnFJXGcACq","cCYApSPiX0","iHwbq2giBx","X5Ebl6sjLf","Rxc2oFCgI4","jFtFONF7NK","b8iIhVOJl6","aGlxlytbic","30e8YSgLzd","0oj65bHiHZ","NtykZPC0DH","qgj6THRgT0","rFM7G7SJbB","lcu5UKrj7w","xurxryxn5u","7XDXFJ8Axh","RuQU6EeJyi","go3SD5CYYT","c8sOoXTa59","Vh0QfTkdKk","rrYBTg4sWc","CmeWiA73ig","iM8MD2IXle","SQk4WlTLiE","wn3JhYo93i","Hlh1Amt1jo","Rd6hiOekcE","LMKIBsBBmQ","nb72pL5LKQ","8GufbXPMRb","OO5OnLzsdP","hLddf4izXl","6x8suntNs6","8VATvVOQZR","jxL8zSOcZN","87YqnuB6Mc","sqr1WbVsWv","fLC17MbW59","tJ6Z5r6mDd","iiEffxACk7","C6w4k9AS2N","9rQTNM8xmG","x4JQLDr6hd","3bYXSm7Zgo","phTYCmMxOW","isl31Fd0SN","CZItvlfdqW"];
+function countCode(letter)
+    console.log (letter)
+for (x = 1;x < data.length ;x++) {
+    if (data.startsWith(letter)) {
+            console.log(data[n])
+    }
+        } 
