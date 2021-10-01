11a826b124d703e186f585d386f20f75788f24f2
Kristofer Järv <90023688+KristoferJarv@users.noreply.github.com>
2021-10-01 08:29:12 +0300

    Update for cycle.js

--- for cycle.js
+++ for cycle.js
@@ -1,1 +1,13 @@
+for (let number = 1; number <=100; number ++) {
+    console.log(number)
+}
 
+for (let x=1; x <=100; x++) {
+    console.log(x)
+}
+
+let x=1;
+while(x <= 100) {
+    console.log(x);
+    x++
+}
